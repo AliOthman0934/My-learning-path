435cbed020e85629dd87c7b2defb506e06512e90
Ali Othman <alioth840@gmail.com>
2024-03-08 11:31:44 +0100

    I added schema to the server file

--- Node.js/REsr API with mongodb/server.js
+++ Node.js/REsr API with mongodb/server.js
@@ -2,20 +2,43 @@ const mongoose = require("mongoose");
 
 const dotenv = require("dotenv");
 
-dotenv.config({path:"./config.env"});
+dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
 
 console.log(process.env);
 
-mongoose.connect(process.env.CONN_STR,{
+mongoose.connect(process.env.CONN_STR, {
 })
-    .then(()=>{
+    .then(() => {
         console.log("DB connection successful")
-})
-    .catch((error)=>{
+    })
+    .catch((error) => {
         console.error("Error connecting to the database:", error);
-})
+    });
+
+const movieSchema =  mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "name is required filed"],
+        unique: true
+    },
+
+    description: String,
+
+    duration: {
+        type: Number,
+        required: [true, "duration is required filed"]
+    },
+
+    rating: {
+        type: Number,
+        default: 1.0
+    },
+
+});
+
+const movieModel = mongoose.model("movie", movieSchema); 
 
 const port = 3000;
 
