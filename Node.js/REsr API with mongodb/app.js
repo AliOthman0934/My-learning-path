208152ea195ff9f85e973f738bca79ad06507cbb
Ali Othman <alioth840@gmail.com>
2024-03-08 10:28:11 +0100

    Connecting to mongodb

---
+++ Node.js/REsr API with mongodb/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const moviesRoute = require("./routes/movieRoute");
+const app = express();
+
+
+app.use(express.json());
+
+// const movieRoute = express.Router();
+
+app.use("/api/v1/movies", moviesRoute)
+
+app.use((req,res,next)=>{
+    req.requestedAt = new Date().toISOString();
+    next();
+})
+
+
+
+module.exports = app
+
+
---
+++ Node.js/REsr API with mongodb/server.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+
+dotenv.config({path:"./config.env"});
+
+const app = require("./app");
+
+console.log(process.env);
+
+mongoose.connect(process.env.CONN_STR,{
+})
+    .then(()=>{
+        console.log("DB connection successful")
+})
+    .catch((error)=>{
+        console.error("Error connecting to the database:", error);
+})
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log("server has started...")
+})
+
